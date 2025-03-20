11e30e34abe4ba0604369d93ce7cef208bd369ed
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2025-03-20 20:29:30 +0300

    final

--- SQL  EDA/eda.sql
+++ SQL  EDA/eda.sql
@@ -1,4 +1,4 @@
--- lets remove null values from the disbursment table
+-- lets remove null values from the disbursment table and extract 
 SELECT TOP (1000) 
       [customer_id],
       [disb_date],
@@ -15,7 +15,7 @@ WHERE [customer_id] IS NOT NULL
   AND [loan_fee] IS NOT NULL;
 
 
-  -- lets remove null values from  the loan repayments table
+  -- lets remove null values from  the loan repayments table and extract 
 
    SELECT TOP (1000) 
       [date_time],
@@ -30,5 +30,28 @@ WHERE [date_time] IS NOT NULL
   AND [rep_month] IS NOT NULL
   AND [repayment_type] IS NOT NULL;
 
+-- lets claulate the profit or losss  and extract it to perfomr forecasing
+SELECT 
+    d.customer_id,
+    d.disb_date,
+    d.account_num,
+    d.loan_amount,
+    d.loan_fee,
+    ISNULL(SUM(r.amount), 0) AS total_repayments,
+    (ISNULL(SUM(r.amount), 0) + d.loan_fee - d.loan_amount) AS profit_loss
+FROM 
+    [credable_customer_transactions].[dbo].[Disbursements$] d
+LEFT JOIN 
+    [credable_customer_transactions].[dbo].[Repayments$] r
+    ON d.customer_id = r.customer_id
+GROUP BY 
+    d.customer_id,
+    d.disb_date,
+    d.account_num,
+    d.loan_amount,
+    d.loan_fee
+ORDER BY 
+    d.customer_id,
+    d.disb_date;
 
 
